dd60ed03fa47d8070df0e1f794a8fef2990e0666
SVR COMPUTERS <svrcomputers.training@gmail.com>
2025-11-19 07:43:47 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -22,9 +22,7 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
       )
     )
   );
